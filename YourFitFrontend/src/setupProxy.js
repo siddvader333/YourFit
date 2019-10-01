8dc8e219e7f2ff0b34a0a652b7861ad30a93634b
Siddharth Vijay <siddharth.vijay333@gmail.com>
2019-10-01 15:49:05 -0400

    Changed proxy method

--- YourFitFrontend/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const proxy = require('http-proxy-middleware');
-
-module.exports = function(app) {
-	app.use(
-		proxy([ '/api', '/auth', '/users', '/api/diagnostic', '/login_success', '/login_failure' ], {
-			target: 'http://localhost:5000'
-		})
-	);
-};
